5f0ba4508216fdd6b88dc88b439aff3b7874ed86
kimhk8321 <kimhk8321@naver.com>
2026-06-28 12:00:00 +0900

    feat: 학습 노트 이관 게시글 29편 + 레이아웃 정리

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   description:
     "프론트엔드 개발과 관련된 학습 기록과 경험을 정리하는 기술 블로그입니다.",
   author: {
-    name: "김형기",
+    name: "hyeonggi",
     email: "kimhk8321@gmail.com",
   },
   locale: "ko_KR",
